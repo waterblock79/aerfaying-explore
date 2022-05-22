d945e738272e3d7ac48bbc2f52ca347b8415ecc6
waterblock79 <33573572+waterblock79@users.noreply.github.com>
2022-05-22 11:24:13 +0800

    🔧 将元数据的 updateURL 从 GreasyFork 地址改为 Github 地址

--- aerfaying-explore.user.js
+++ aerfaying-explore.user.js
@@ -4,7 +4,7 @@
 // @version      0.1.2
 // @description  提供优化、补丁及小功能提升社区内的探索效率和用户体验
 // @author       waterblock79
-// @updateURL    https://greasyfork.org/scripts/445198-%E9%AB%98%E6%95%88%E6%8E%A2%E7%B4%A2-%E9%98%BF%E5%84%BF%E6%B3%95%E8%90%A5-%E7%A8%BD%E6%9C%A8%E4%B8%96%E7%95%8C%E7%A4%BE%E5%8C%BA%E4%BC%98%E5%8C%96%E6%8F%92%E4%BB%B6/code/%E9%AB%98%E6%95%88%E6%8E%A2%E7%B4%A2%20-%20%E9%98%BF%E5%84%BF%E6%B3%95%E8%90%A5%E7%A8%BD%E6%9C%A8%E4%B8%96%E7%95%8C%E7%A4%BE%E5%8C%BA%E4%BC%98%E5%8C%96%E6%8F%92%E4%BB%B6.user.js
+// @updateURL    https://github.com/waterblock79/aerfaying-explore/raw/main/aerfaying-explore.user.js
 // @match        http*://gitblock.cn/*
 // @match        http*://aerfaying.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gitblock.cn
