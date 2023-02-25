112232991fa55e9f65fac884bb55934a03ea6df4
waterblock79 <33573572+waterblock79@users.noreply.github.com>
2023-02-26 00:16:27 +0800

    修复版本号的问题；该版本进行了若干优化，重构了本地搜索、添加了Markdown沙盒功能，详见上个Commit。

--- aerfaying-explore.user.js
+++ aerfaying-explore.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Aerfaying Explore - 阿儿法营/稽木世界社区优化插件
 // @namespace    waterblock79.github.io
-// @version      1.11.3
+// @version      1.12.0
 // @description  提供优化、补丁及小功能提升社区内的探索效率和用户体验
 // @author       waterblock79
 // @match        http://gitblock.cn/*
@@ -32,7 +32,7 @@
             alert('似乎无法在您的浏览器上运行此脚本。')
         }
     }
-    const version = '1.11.3';
+    const version = '1.12.0';
 
     if (location.search === '?NoUserscript') return;
 
