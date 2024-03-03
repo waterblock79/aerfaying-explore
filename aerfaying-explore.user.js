8ec5552714aeacc21457a0366ced37e6732fe5dc
waterblock79 <waterblock79@outlook.com>
2024-03-03 18:14:04 +0800

    🐛 修复自定义主题颜色 .btn-outline-primary:hover 的文字颜色问题

--- aerfaying-explore.user.js
+++ aerfaying-explore.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Aerfaying Explore - 阿儿法营/稽木世界社区优化插件
 // @namespace    waterblock79.github.io
-// @version      1.17.2
+// @version      1.17.3
 // @description  提供优化、补丁及小功能提升社区内的探索效率和用户体验
 // @author       waterblock79
 // @match        http://gitblock.cn/*
@@ -32,7 +32,7 @@
             alert('似乎无法在您的浏览器上运行此脚本。')
         }
     }
-    const version = '1.17.2';
+    const version = '1.17.3';
 
     if (location.search === '?NoUserscript') return;
 
@@ -2413,7 +2413,7 @@
             }
             .btn-outline-primary:hover {
                 background-color: ${mainColor()} !important;
-                color: #fff;
+                color: #fff !important;
             }
             .modal_modal-overlay_2_Dgx {
                 background-color: ${mainColorRGB(1, 0.9)} !important;
